ac4dab17d1ec1b87d5076b5a210ba76cc3cc3e28
Lukas Lösche <lukas@some.engineering>
2024-03-05 18:20:17 +0100

    Add dev tools

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-.PHONY: all generate_in requirements.txt requirements-test.txt
+.PHONY: all generate_in requirements.txt requirements-test.txt requirements-dev.txt
 
-all: generate_in requirements.txt requirements-test.txt
+all: generate_in requirements.txt requirements-test.txt requirements-dev.txt
 
 generate_in:
 	python tools/make-requirements-in.py
@@ -10,3 +10,6 @@ requirements.txt: requirements.in pyproject.toml
 
 requirements-test.txt: requirements-test.in pyproject.toml
 	pip-compile -q --no-annotate --resolver=backtracking --upgrade --allow-unsafe --no-header  --unsafe-package n/a --no-strip-extras requirements-test.in -o requirements-test.txt
+
+requirements-dev.txt: requirements-dev.in pyproject.toml
+	pip-compile -q --no-annotate --resolver=backtracking --upgrade --allow-unsafe --no-header  --unsafe-package n/a --no-strip-extras requirements-dev.in -o requirements-dev.txt
--- tools/make-requirements-in.py
+++ tools/make-requirements-in.py
@@ -8,17 +8,23 @@ def create_in_files(pyproject_path: str):
         pyproject = tomllib.load(f)
 
     dependencies = pyproject.get("project", {}).get("dependencies", [])
-    optional_dependencies = pyproject.get("pyproject", {}).get("optional-dependencies", {}).get("test", [])
+    test_dependencies = pyproject.get("pyproject", {}).get("optional-dependencies", {}).get("test", [])
+    dev_dependencies = pyproject.get("pyproject", {}).get("optional-dependencies", {}).get("dev", [])
 
     # Write main dependencies to requirements.in
     with open("requirements.in", "w") as f:
         for dep in dependencies:
             f.write(dep + "\n")
 
-    # Write test dependencies to requirements-test.in (includes extras)
+    # Write test dependencies to requirements-test.in
     with open("requirements-test.in", "w") as f:
-        for test_dep in optional_dependencies:
+        for test_dep in test_dependencies:
             f.write(test_dep + "\n")
+
+    # Write dev dependencies to requirements-dev.in
+    with open("requirements-dev.in", "w") as f:
+        for dev_dep in dev_dependencies:
+            f.write(dev_dep + "\n")
 
 
 if __name__ == "__main__":
