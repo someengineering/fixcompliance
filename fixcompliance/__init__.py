5e7309918dcf7e21a6c69d12652e5eff8ed648b4
Some CI <ci@some.engineering>
2024-06-24 09:43:11 +0000

    Bump version to 0.4.22

--- fixcompliance/__init__.py
+++ fixcompliance/__init__.py
@@ -6,7 +6,7 @@ from typing import Any
 from importlib.resources import files as resource_filename
 from pathlib import Path
 
-__version__ = "0.4.21"
+__version__ = "0.4.22"
 
 Json = dict[str, Any]
 CACHE_TIMEOUT = 3600
